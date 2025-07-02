0b27dcceb20f04497eafff3cfb4dd08171cf26dc
imsudipcreator <imsudipmahata@gmail.com>
2025-07-02 21:41:08 +0530

    05: ai-jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,25 @@
 import { inngest } from "./client";
+import { openai, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "30s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+  async ({ event }) => {
+    // Create a new agent with a system prompt (you can add optional tools, too)
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert Next.js developer. You write readable, maintainable code. You write simple Next.js and React snippets",
+      model: openai({
+        model: "meta-llama/llama-4-maverick-17b-128e-instruct",
+        apiKey: process.env.GROQ_API_KEY,
+        baseUrl: "https://api.groq.com/openai/v1",
+      }),
+    });
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`
+    );
+    console.log(output);
+    // [{ role: 'assistant', content: 'function removeUnecessaryWhitespace(...' }]
+    return { message: output };
+  }
 );
